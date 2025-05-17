1b35e354fc7b68f7400b27c391a9561781643353
shefffran <120892391+shefffran@users.noreply.github.com>
2025-05-17 23:05:30 +0400

    Update abl.tf

--- Challend_high-avalability-backet/ec2/abl.tf
+++ Challend_high-avalability-backet/ec2/abl.tf
@@ -18,8 +18,13 @@ resource "aws_alb_listener" "alb_listener" {
   protocol = "HTTP"
 
   default_action {
-    type =  "forward"
-    target_group_arn = aws_lb_target_group.alb_tg.arn
+  type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
